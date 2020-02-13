3f18b7abb6c85fcfef899540e9013b850ffb0eb8
HuangFeiHong <1605121333@qq.com>
2020-02-13 20:07:54 +0800

    hello

---
+++ DataStructure/src/com/haha/array/Demo.java
@@ -0,0 +1,12 @@
+package com.haha.array;/**
+ * @Author:HuangFeiHong
+ * @Date:2020/2/13
+ * @Description:
+ */
+
+/**
+ @auther HuangFeiHong
+ @date 2020-02-13 18:47 
+ */
+public class Demo {
+}
---
+++ DataStructure/src/com/haha/array/Demo2.java
@@ -0,0 +1,30 @@
+package com.haha.array;
+
+import java.util.List;
+
+/**
+ * @Author:HuangFeiHong
+ * @Date:2020/2/13
+ * @Description:
+ */
+public class Demo2 {
+
+    /**
+     *@Author:  HuangFeiHong
+     *@Description:
+     *@Params: [name, password]
+     *@ReturnValue : java.util.List<java.lang.Object>
+     *@Date:  2020/2/13
+     *@ModifiedByAndOtherInformation:
+
+     */
+    public List<Object> show(String name, String password){
+
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i);
+        }
+
+
+        return null;
+    }
+}
